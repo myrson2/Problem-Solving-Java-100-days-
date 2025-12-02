a5247dbb1422691a381e18c71d67447d500901f2
Myrson2 <beros.josemyrson.oreo1@gmail.com>
2025-12-02 17:49:18 +0800

    day19

---
+++ day19.java
@@ -0,0 +1,29 @@
+//Problem: First Unique Character in a String
+
+import java.util.HashMap;
+
+public class day19 {
+    static int firstUniqueCharacterInAString(String str){
+        HashMap<Character, Integer> map = new HashMap<>();
+        int storeIndex = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+         for (int j = 0; j < str.length(); j++) {
+                if(map.getOrDefault(str.charAt(j), 0) == 1){
+                    storeIndex = j;
+                    break;
+                } 
+           }
+        
+        return storeIndex;
+    }
+    public static void main(String[] args) {
+        String input = "aabbc";
+
+        System.out.println(firstUniqueCharacterInAString(input.toLowerCase()));;
+    }
+}
