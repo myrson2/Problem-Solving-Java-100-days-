31c1f5df98ebc75e851c2f9681a824df1a916137
Myrson2 <beros.josemyrson.oreo1@gmail.com>
2025-12-04 17:57:25 +0800

    finisher day17c

--- day17C.java
+++ day17C.java
@@ -1,16 +1,28 @@
 //Day 17C — Longest Substring With No More Than Two Repeating Characters
 
-import java.util.HashMap;
-
 public class day17C {
     static int lengthOfSubstringMaxTwoRepeats(String s){
-        HashMap<Character, Integer> map = new HashMap<>();
-        int left = 0, maxLength = 0, consecutiveLimit = 2;
+        int left = 0, maxLength = 0, consecutiveLimit = 0;
+        char prev = ' ';
 
         for(int right = 0; right < s.length(); right++){
             char c = s.charAt(right);
-            map.put(c, map.getOrDefault(c, 0) + 1);
+
+            if(prev ==  c){
+                consecutiveLimit++;
+            } else {
+                consecutiveLimit = 1;
+                prev = c;
+            }
+
+            if(consecutiveLimit > 2){
+                left++;
+                consecutiveLimit--;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
         }
+
+        return maxLength;
     }
     public static void main(String[] args) {
 
